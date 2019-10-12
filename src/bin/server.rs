2f8cd4243a1587b341b4f6f13bce9709ac1783af
Quentin Boyer <qbsecond@gmail.com>
2019-10-12 13:29:10 +0200

    Moar things

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -6,6 +6,36 @@ pub enum Action {}
 
 use tokio::sync::mpsc;
 
+struct SshHandler;
+struct ArduinoHandler {
+    stream: tokio::net::TcpStream,
+}
+impl ArduinoHandler {
+    fn send(&mut self, command: ArduinoCommand) {}
+}
+
+enum ArduinoCommand {
+    Set { id: i8, state: bool },
+    Toggle { id: i8 },
+}
+
+impl ArduinoCommand {
+    fn repr(&self) -> String {
+        match self {
+            ArduinoCommand::Set { id, state: true } => format!("on{}\n", id),
+            ArduinoCommand::Set { id, state: false } => format!("off{}\n", id),
+            ArduinoCommand::Toggle { id } => format!("tog{}\n", id),
+        }
+    }
+}
+
+impl ArduinoHandler {}
+
+enum Handler {
+    Arduino(ArduinoHandler),
+    SSH(SshHandler),
+}
+
 fn main() {
     simplelog::TermLogger::init(
         simplelog::LevelFilter::Debug,
